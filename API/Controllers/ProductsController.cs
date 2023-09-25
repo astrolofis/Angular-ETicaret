93760d896b180fde3a3cb7a5120f299b8a6bbadc
Mehmet Muharrem Ceylan <mehmetmuharrem1919@gmail.com>
2023-09-25 20:07:34 +0300

    Proje dosyası ekle.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data.DataContext;
+using API.Data.DbModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+        /// <summary>
+        /// Database'deki verileri çekme.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]//http verbs -> get post delete put
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var data = await _context.Products.ToListAsync();
+            return data;
+        }
+        /// <summary>
+        /// Database'deki belirli bir veriyi çekme.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
