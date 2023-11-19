5be20b535860c5c12a16f1f651cdd371964000aa
Mehmet Muharrem Ceylan <mehmetmuharrem1919@gmail.com>
2023-11-19 16:55:53 +0300

    Angular ortam hazırlanması ve UI shop işlemleri (Modüller, Ürün listeleme ekranı ve işlemleri, Componente veri aktarma, Ürün tip marka vb. gösterimi, Sıralama , Sayfalama ve Search işlemleri)

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,6 +29,13 @@ namespace API
             services.AddDbContext<StoreContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+                });
+            });
             
 
         }
@@ -45,9 +52,13 @@ namespace API
 
             app.UseStatusCodePagesWithReExecute("/error/{0}");
 
+            app.UseStaticFiles();
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
