d8be8f9e4b347aa288d81ad834eac6ededac39a5
Mehmet Muharrem Ceylan <mehmetmuharrem1919@gmail.com>
2023-12-24 13:49:28 +0300

    Revert "Routing işlemleri/ hata ayıklama ve hata mesajları / yükleniyor ekranı / breadCrumb eklendi"
    This reverts commit 860be9a1f883b73d894a9497be2fa1463852ce6a.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { HomeComponent } from './home/home.component';
 import { ShopComponent } from './shop/shop.component';
 import { ProductDetailsComponent } from './shop/product-details/product-details.component';
@@ -19,6 +20,10 @@ const routes: Routes = [
   {path:'shop/:id',component: ProductDetailsComponent, data:{breadcrumb:{alias:'shopDetail'}}},
   {path:'**',redirectTo: '',pathMatch:'full'}
 ];
+=======
+
+const routes: Routes = [];
+>>>>>>> parent of 860be9a (Routing işlemleri/ hata ayıklama ve hata mesajları / yükleniyor ekranı / breadCrumb eklendi)
 =======
 
 const routes: Routes = [];
