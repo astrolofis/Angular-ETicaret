e6e8d7d560912be59f3cf52b4c9f8884b75a52fe
Mehmet Muharrem Ceylan <mehmetmuharrem1919@gmail.com>
2023-12-24 12:56:11 +0300

    hatalı bölüm

---
+++ API.Core/DbModels/CustomerBasket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Core.DbModels
+{
+    public class CustomerBasket : BaseEntity
+    {
+        public CustomerBasket()
+        {
+            
+        }
+
+        public CustomerBasket(string id)
+        {
+            Id = id;
+        }
+
+        public new string Id { get; set; }
+
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+    }
+}
